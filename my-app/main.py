a640f647175fbf1fd863d1ae16e48322ea91a679
gregorio <gregorio.rico.gregorio@gmail.com>
2023-11-21 21:49:17 -0600

    Inicio
    Creacion de proyecto con flet

--- my-app/main.py
+++ my-app/main.py
@@ -1,36 +1,23 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Inicio de Sesión"
+    def login_click(e):
+        # Aquí iría la lógica para validar el inicio de sesión
+        # Por ahora, solo redirigimos a la página del calendario
+        page.controls.clear()
+        create_calendar_page(page)
 
-    # Function to handle the button click event
-    def on_login_click(e):
-        print("Botón de inicio de sesión presionado")
+    # Controles para el login
+    txt_username = ft.TextField(label="Nombre de usuario", autofocus=True)
+    txt_password = ft.PasswordField(label="Contraseña")
+    btn_login = ft.Button("Iniciar sesión", on_click=login_click)
 
-    # Create a user image widget with a placeholder for now
-    user_image = ft.Image(src="url_or_path_to_user_image", width=100, height=100)
+    # Agregando controles a la página
+    page.add(txt_username, txt_password, btn_login)
 
-    # Create text fields for username and password
-    username = ft.TextField(label="Nombre de Usuario", autofocus=True)
-    password = ft.TextField(label="Contraseña", password=True)
+def create_calendar_page(page: ft.Page):
+    # Aquí construirás la interfaz del calendario
+    page.add(ft.Text("Bienvenido al Calendario"))
 
-    # Create a button for submitting the login form
-    login_button = ft.ElevatedButton(text="Iniciar Sesión", on_click=on_login_click)
-
-    # Create a column for the form elements
-    form_elements = ft.Column(controls=[
-        user_image, username, password, login_button
-    ])
-
-    # Create a row that centers the column on the page
-    centered_row = ft.Row(controls=[
-        ft.Expanded(), form_elements, ft.Expanded()
-    ], expand=True)
-
-    # Add some padding around the row to push it to the center of the page vertically
-    centered_content = ft.Padding(padding=ft.EdgeInsets.all(16), content=centered_row)
-
-    # Add the padded row to the page
-    page.add(centered_content)
-
+# Ejecutar la aplicación
 ft.app(target=main)
